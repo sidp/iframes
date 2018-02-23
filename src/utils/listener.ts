d4de7d5926a01a310aebddea02ca7b6551c320a0
Peter Simonsson <peter@simonsson.com>
2018-02-23 14:18:47 +0100

    Added Listener#off

--- src/utils/listener.ts
+++ src/utils/listener.ts
@@ -9,10 +9,10 @@ export class Listener {
 	listeners = [];
 
 	constructor(on: Window) {
-		on.addEventListener('message', this.handleIncomingMessage);
+		on.addEventListener('message', this.handleMessages);
 	}
 
-	handleIncomingMessage = (ev: MessageEvent) => {
+	handleMessages = (ev: MessageEvent) => {
 		// todo: validate where message comes from
 		const msg = ev.data as IMessage;
 		this.listeners
@@ -20,9 +20,18 @@ export class Listener {
 			.forEach(listener => listener.fn(msg));
 	};
 
-	on(type: MessageType, fn: (msg: IMessage) => void) {
+	on = (type: MessageType, fn: (msg: IMessage) => void) => {
+		if (typeof fn !== 'function') {
+			throw new Error(
+				`Listener#on(type, fn) requires the second argument to be a function. ${typeof fn} provided.`
+			);
+		}
 		this.listeners.push({ type, fn });
-	}
+	};
+
+	off = (type: MessageType, fn: (msg: IMessage) => void) => {
+		this.listeners = this.listeners.filter(listener => listener.fn !== fn);
+	};
 }
 
 export default function listener(on: Window) {
