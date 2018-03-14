f3149267894c4bbd7e911b038dd5a2b970b0b5fb
Peter Simonsson <peter@simonsson.com>
2018-03-14 18:16:44 +0100

    Prepare package

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,5 @@ import setup from './parent/setup';
 import * as parentWindow from './child/window';
 import * as iframe from './child/iframe';
 
-const iframeApi = {
-	setup,
-	parentWindow,
-	iframe,
-};
-
-(<any>window).sidpIframe = iframeApi;
-export default iframeApi;
+(<any>window).sidpIframe = { setup, parentWindow, iframe };
+export { setup, parentWindow, iframe };
